7dee07158b47edc5566eedc8021158afd8dbf1fb
Francaosawaru <forierie@yahoo.com>
2024-03-24 12:05:50 +0000

    web flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,36 +1,36 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+    This module starts a simple flask application and sets the routes
+    /, /hbnb, /c/<text>, /python/<text> route
 """
 
 from flask import Flask
+from markupsafe import escape
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+def web_root():
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """returns HBNB"""
+def hbnb_route():
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+def c_route(text):
+    return 'C %s' % escape(text.replace('_', ' '))
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
-    return 'Python ' + text.replace('_', ' ')
+def python_route(text="is cool"):
+    return 'Python {}'.format(escape(text.replace("_", " ")))
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
-
+    app.run(host='0.0.0.0', port=5000)
