71ea0ee4a613300de0a3700962466d8ce7b68fbb
Francaosawaru <forierie@yahoo.com>
2024-03-24 19:47:47 +0000

    web flask

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,46 +1,33 @@
 #!/usr/bin/python3
-"""
-    This module starts a simple flask application and sets the
-    /states_list route to display a list of all states in the db
-"""
-
-from flask import Flask, render_template
-from markupsafe import escape
-from models import storage
-from models.state import State
-
+""" a script that starts a Flask web application """
+from flask import Flask
+from flask import render_template
+from models import storage, State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ Closes the storage session """
-
+def remove_session(exception):
+    """ After each request, it removes the current SQLAlchemy Session """
     storage.close()
 
 
 @app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def states_display(id=None):
-    """ States list of all dump, all_states is a dictionary containing """
+def render_states():
+    """ displays all states """
+    States = storage.all(State).values()
+    return render_template("9-states.html", States=States, one=None)
 
-    states_dict = storage.all(State)
 
-    new_dict = {value.id: value.name for value in states_dict.values()}
-    if not id:
-        return render_template('7-states_list.html', all_states=new_dict)
-
-    state_id = str(id)
-    if id in new_dict.keys():
-        return render_template(
-                '9-states.html',
-                all_states=states_dict["State.{}".format(state_id)])
-    else:
-        return render_template(
-                '9-states.html',
-                all_states=None)
-
+@app.route('/states/<string:id>', strict_slashes=False)
+def render_one_state(id):
+    """ displays one state if it exists """
+    key = "State." + id
+    one = None
+    if key in storage.all(State):
+        one = storage.all(State)[key]
+    return render_template("9-states.html", States=None, one=one)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
