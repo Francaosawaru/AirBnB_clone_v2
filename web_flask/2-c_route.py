a362eb268ca0e5239e5c8dc3e4e96d32e576e07c
Francaosawaru <forierie@yahoo.com>
2024-03-24 12:02:46 +0000

    web flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application C is FUN"""
+"""
+    This module starts a simple flask application
+    and sets the routes /, /hbnb, /c/<text> route
+"""
+
 from flask import Flask
+from markupsafe import escape
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Prints a Message when / is called """
+def web_root():
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """ Prints a Message when /hbnb is called """
+def hbnb_route():
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """ Prints a Message when /c is called """
-    return "C " + text.replace('_', ' ')
+def c_route(text):
+    return 'C %s' % escape(text.replace('_', ' '))
 
-if __name__ == "__main__":
-    """ Main Function """
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
