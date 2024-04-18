e6f98d5307535229ca9952a910b2a4a077f4c740
Francaosawaru <forierie@yahoo.com>
2024-04-18 10:58:15 +0000

    updated

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
-    This module starts a simple flask application
+
+start Flask application
 """
 
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def web_flask():
-    return 'Hello HBNB!'
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def index():
+        """returns Hello HBNB!"""
+            return 'Hello HBNB!'
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+            if __name__ == '__main__':
+                app.run(host='0.0.0.0', port='5000')
